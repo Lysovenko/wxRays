5a6d038581e0dece5ed9ea02f502d09da6c4a50c
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2016-03-30 16:22:25 +0300

    it is impossible to work wo regexps

--- utils/starobj.py
+++ utils/starobj.py
@@ -16,6 +16,8 @@
 # along with this program; if not, write to the Free Software Foundation,
 # Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+import re
+
 class StarObj(dict):
     def __init__(self, initor=None):
         dict.__init__(self)
